cfb26003735d256cc59d78c50068ab3cf8f3e6ee
Rajps44 <singhayush88805@gmail.com>
2024-08-24 12:50:46 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -18,7 +18,7 @@ class Var(object):
     name = str(getenv('name','Netflixmovielakh_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002244711970'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001924382977'))
     NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001924382977'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))

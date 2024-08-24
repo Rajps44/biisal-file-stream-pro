04a75e6c190ffc8d7f7b649b63eb17069d6b446b
Rajps44 <singhayush88805@gmail.com>
2024-08-24 22:47:54 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -1,57 +1,62 @@
 import os
-from os import getenv, environ
 from dotenv import load_dotenv
 import logging
 
 # Setup logging
 logging.basicConfig(level=logging.INFO)
 
+# Load environment variables from a .env file
 load_dotenv()
-bot_name = "Netflixmovielakh_bot"
-bisal_channel = "https://t.me/netfilixmo_ch"
-bisal_grp = "https://t.me/n_flixmovie"
 
 class Var(object):
-    MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '27002519'))
-    API_HASH = str(getenv('API_HASH', '1033ee721101d78366b4ac46aadf3930'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , '7478730845:AAGTeTsmm7ToeQc7lW3jimEPiAlddFySsNo'))
-    name = str(getenv('name','Netflixmovielakh_bot'))
-    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
-    WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001924382977'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001924382977'))
-    PORT = int(getenv('PORT', '8080'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "6508598835").split()]
-    NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Rajps33'))
+    # Bot configuration
+    bot_name = "Netflixmovielakh_bot"
+    bisal_channel = "https://t.me/netfilixmo_ch"
+    bisal_grp = "https://t.me/n_flixmovie"
     
-    if 'DYNO' in environ:
+    MULTI_CLIENT = False
+    API_ID = int(os.getenv('API_ID', '27002519'))
+    API_HASH = str(os.getenv('API_HASH', '1033ee721101d78366b4ac46aadf3930'))
+    BOT_TOKEN = str(os.getenv('BOT_TOKEN', '7478730845:AAGTeTsmm7ToeQc7lW3jimEPiAlddFySsNo'))
+    name = str(os.getenv('name', 'Netflixmovielakh_bot'))
+    SLEEP_THRESHOLD = int(os.getenv('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(os.getenv('WORKERS', '4'))
+    BIN_CHANNEL = int(os.getenv('BIN_CHANNEL', '-1001924382977'))
+    NEW_USER_LOG = int(os.getenv('NEW_USER_LOG', '-1001924382977'))
+    PORT = int(os.getenv('PORT', '8080'))
+    BIND_ADDRESS = str(os.getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    PING_INTERVAL = int(os.getenv('PING_INTERVAL', '1200'))  # 20 minutes
+    OWNER_ID = [int(x) for x in os.getenv("OWNER_ID", "6508598835").split()]
+    NO_PORT = bool(os.getenv('NO_PORT', 'False') == 'True')
+    APP_NAME = None
+    OWNER_USERNAME = str(os.getenv('OWNER_USERNAME', 'Rajps33'))
+
+    if 'DYNO' in os.environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME'))  # don't need to fill anything here
-        API = environ.get("API", "fa2a0768fc8d2a51b22e46293634a52670a73c7a")  # shortlink API
-        URL = environ.get("URL", "shortxlinks.com")  # shortlink domain without https://
-        VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "")  # how to open link
-        VERIFY = environ.get("VERIFY", "True")
+        APP_NAME = str(os.getenv('APP_NAME', ''))  # The app name on Heroku, if any
+        API = os.getenv("API", "fa2a0768fc8d2a51b22e46293634a52670a73c7a")  # Shortlink API
+        URL = os.getenv("URL", "shortxlinks.com")  # Shortlink domain without https://
+        VERIFY_TUTORIAL = os.getenv("VERIFY_TUTORIAL", "")  # How to open the verification link
+        VERIFY = os.getenv("VERIFY", "True")  # Verification setting
     else:
         ON_HEROKU = False
-    
-    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
-    HAS_SSL = bool(getenv('HAS_SSL', True))
-    
+
+    # Determine FQDN (Fully Qualified Domain Name) based on whether it's running on Heroku or not
+    FQDN = str(os.getenv('FQDN', 'BIND_ADDRESS:PORT')) if not ON_HEROKU or os.getenv('FQDN', '') else APP_NAME + '.herokuapp.com'
+    HAS_SSL = bool(os.getenv('HAS_SSL', 'True') == 'True')  # Check if SSL is enabled
+
+    # Set the URL with or without SSL
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://t54s2lqiv6:2mOV4n1iL21cMcMH@cluster0.ma3sm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'netfilixmo_ch'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1002244711970")).split()))
-    BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))
-    BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.Pʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ @biisal_bot ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
 
-# लॉगिंग जोड़ें
-logging.info(f"VERIFY value: {VERIFY}")
+    # MongoDB database URL
+    DATABASE_URL = str(os.getenv('DATABASE_URL', 'mongodb+srv://t54s2lqiv6:2mOV4n1iL21cMcMH@cluster0.ma3sm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
+    UPDATES_CHANNEL = str(os.getenv('UPDATES_CHANNEL', 'netfilixmo_ch'))
+    BANNED_CHANNELS = list(set(int(x) for x in str(os.getenv("BANNED_CHANNELS", "-1002244711970")).split()))
+    BAN_CHNL = list(set(int(x) for x in str(os.getenv("BAN_CHNL", "")).split()))
+    BAN_ALERT = str(os.getenv('BAN_ALERT', '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.Pʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ @biisal_bot ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
+
+# Log the value of VERIFY to ensure it is loaded correctly
+logging.info(f"VERIFY value: {Var.VERIFY}")
